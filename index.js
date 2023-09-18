35006f1861cc7caa6aa8a40c1e062f9dc464d7e1
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-18 19:37:24 +0300

    Add resources - css, images...

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+const express = require('express');
+
+const app = express();
+const port = 5500;
+
+app.get('/', (req, res) => {
+    res.send('Zdrasti pich')
+});
+
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
