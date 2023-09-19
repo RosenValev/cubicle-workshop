db473971a4c9e947ce28f60520ea2915b46efe3a
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-19 09:58:42 +0300

    Add handlebars to project

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 5500;
 
+//Handlebars config
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views')   //setting correct views folder, because it`s nested in src folder, not main folder
+
 app.get('/', (req, res) => {
-    res.send('Zdrasti pich')
+    res.render('index')
 });
 
 
