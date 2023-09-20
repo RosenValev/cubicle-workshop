5c0e5d204b9acbca972e091b3b505ce39b302c3e
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-20 10:35:41 +0300

    Adding cubes on homepage

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,20 +1,6 @@
 const uniqId = require('uniqid');
-
 const cubes = [
-    {
-        id: '1n73sh8holhz66elc',
-        name: 'Mirror Cube',
-        description: 'A cool mirror cube',
-        imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.jpg',
-        difficultyLevel: 4
-    },
-    {
-        id: '2n73sh8holaz66elc',
-        name: 'Rubic Classic',
-        description: 'Evergreen',
-        imageUrl: 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg',
-        difficultyLevel: 3
-    }
+
 ];
 
 exports.create = (cubeData) => {
@@ -31,6 +17,7 @@ exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
 exports.getAll = (search, from, to) => {
     let result = cubes.slice();
+    console.log(result);
 
     if (search) {
         result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
@@ -43,4 +30,6 @@ exports.getAll = (search, from, to) => {
     if (to) {
         result.filter(cube => cube.difficultyLevel <= Number(to));
     }
+
+    return result;
 };
