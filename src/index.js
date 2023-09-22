a5f2ffbf26ebc58ea224dec0303344e835563fae
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-22 14:14:07 +0300

    P2 - mongoose init and DB configured.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const handlebarsConfig = require('./config/handlebarsConfig.js');
 const expressConfig = require('./config/expressConfig.js');
-const routes = require('./routes.js')
+const routes = require('./routes.js');
+const dbConnect = require('./config/dbConfig.js');
+
 
 const app = express();
 const port = 5500;
 
 handlebarsConfig(app);
 expressConfig(app);
+
+dbConnect()
+    .then(() => console.log('DB connected successfully'))
+    .catch(err => console.log('DB error :', err));
+
 
 app.use(routes)
 
