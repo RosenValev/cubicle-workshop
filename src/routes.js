e3b254f3e4f1245547f3d844996cf4474ce19496
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-22 19:23:49 +0300

    P2 - Add accessory template

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController.js');
 const cubeController = require('./controllers/cubeController.js');
+const accessoryController = require('./controllers/accessoryController.js');
 
 router.use(homeController);   // This controller will work all the time.
 router.use('/cubes', cubeController); // This controller will work only when we have req to /cubes/
+router.use('/accessories', accessoryController);
 router.get('*', (req, res) => {  //Redirect everything else to page 404
     res.redirect('/404')
 });
