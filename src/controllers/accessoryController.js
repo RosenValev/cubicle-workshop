686bd6ca0540967e1042650829808138dac72a8f
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-22 19:32:28 +0300

    P2 - Add post action for create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -4,4 +4,13 @@ router.get('/create', (req, res) => {
     res.render('accessory/create');
 })
 
+router.post('/create', (req, res) => {
+    const { name, description, imageUrl } = req.body
+    console.log({ name, description, imageUrl })
+
+    // TODO Add accessory to DB
+    res.redirect('/');
+
+});
+
 module.exports = router;
