41999eee191135dbfa09d817af79bd078c38f7d4
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-23 13:18:43 +0300

    P2 - Display all available accessory in attach page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -7,7 +7,7 @@ router.get('/create', (req, res) => {
 
 router.post('/create', async (req, res) => {
     const { name, description, imageUrl } = req.body
-    console.log({ name, description, imageUrl })
+    // console.log({ name, description, imageUrl })
 
     await accessoryService.create({ name, description, imageUrl });
 
--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const cubeService = require('../services/cubeService.js');
+const accessoryService = require('../services/accessoryService.js');
 
 router.get('/create', (req, res) => {     // Path ...../cubes/create, because of the modular routes.
     res.render('create');
@@ -34,8 +35,10 @@ router.get('/:cubeId/details', async (req, res) => {
 });
 
 
-router.get('/:cubeId/attach-accessory', (req, res) => {
-    res.render('accessory/attach');
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const cube = await cubeService.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryService.getAll().lean();
+    res.render('accessory/attach', { cube, accessories });
 });
 
 
