b53184ceb9d07265da3cb942c3a585b3532c386f
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-23 16:35:58 +0300

    P2 - show remaining accessories which could be attached

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -3,3 +3,5 @@ const Accessory = require('../models/Accessory.js');
 exports.create = (accessoryData) => Accessory.create(accessoryData);
 
 exports.getAll = () => Accessory.find();
+
+exports.getNotAttached = (accessoriesId) => Accessory.find({ _id: { $nin: accessoriesId } });
