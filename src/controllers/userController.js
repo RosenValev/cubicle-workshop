8076d3636745135ff9d2d6f33e7fea6ba14f88b7
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-27 21:22:58 +0300

    P3 - Created userSchema and model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,4 +5,12 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
+router.post('/register', (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+
+    console.log({username, password, repeatPassword})
+
+    res.redirect('users/login');
+});
+
 module.exports = router;
