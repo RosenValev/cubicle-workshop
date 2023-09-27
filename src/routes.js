46443f76db87872cef34de094423ae14da458c1b
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-09-27 21:14:09 +0300

    P3 - Add register page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController.js');
 const cubeController = require('./controllers/cubeController.js');
 const accessoryController = require('./controllers/accessoryController.js');
+const userController = require('./controllers/userController.js');
 
 router.use(homeController);   // This controller will work all the time.
 router.use('/cubes', cubeController); // This controller will work only when we have req to /cubes/
 router.use('/accessories', accessoryController);
+router.use('/users', userController);
 router.get('*', (req, res) => {  //Redirect everything else to page 404
     res.redirect('/404')
 });
